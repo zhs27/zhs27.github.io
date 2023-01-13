d25ec767798981b350bab9edf708bdc02198bffb
Haosheng Zhang <zhanghaosheng2000@163.com>
2023-01-12 22:01:51 -0500

    Multiple courses

--- coursePlanner.js
+++ coursePlanner.js
@@ -15,50 +15,89 @@
     </h1>
  
     <h3 id="head">Input Courses</h3>
+    <p id="courseNSection">Inputting:Course1 Section1</p>
     <form class="" action="index.html" method="post">
-        <input type="text" name="course1section1[]" value="" /><br>
-        <input type="text" name="course1section1[]" value="" /><br>
-        <input type="text" name="course1section1[]" value="" /><br>
-        <input type="text" name="course2section2[]" value="" /><br>
-        <input type="text" name="course2section2[]" value="" /><br>
-        <input type="text" name="course2section2[]" value="" /><br>
-        <input type="text" name="course1section1[]" value="" /><br>
-        <input type="text" name="course1section1[]" value="" /><br>
-        <input type="text" name="course1section1[]" value="" /><br>
-        <input type="text" name="course2section2[]" value="" /><br>
-        <input type="text" name="course2section2[]" value="" /><br>
-        <input type="text" name="course2section2[]" value="" /><br>
-        <button type="button" name="button" onclick="planner()">
-            Submit
+        <input type="text" name="day" value="" /><br>
+        <input type="text" name="starttime" value="" /><br>
+        <input type="text" name="endTime" value="" /><br>
+        <button type="button" name="button" onclick="sectionAdd()">
+            next section
+        </button>
+        <button type="button" name="button" onclick="courseAdd()">
+            next course
+        </button>
+        <button type="button" name="button" onclick="planner(0)">
+            submit
         </button>
     </form>
     <br>
  
     <p id="par"></p>
+    var sections;
+    var courses;
+    var course = 1;
+    var section = 1;
+
+    <script type="text/javascript">
+        function sectionAdd() {
+            var day = document.getElementsByName('day');
+            if (day.value !="") {
+                day.value = "";
+            }
+            var startTime = document.getElementsByName('startTime');
+            if (startTime.value !="") {
+                startTime = "";
+            }
+            var endTime = document.getElementsByName('endTime');
+            if (endTime.value !="") {
+                endTime = "";
+            }
+            var thisSection = [day, startTime, endTime];
+            sections.push(thisSection);
+            section = section + 1;
+            document.getElementById("courseNSection").innerHTML = "Inputting: Course" + course + " Section" + section;
+        }
+    </script>
+
+    <script type="text/javascript">
+        function courseAdd() {
+            sectionAdd();
+            courses.push(sections);
+            course = course + 1;
+            section = 1;
+            document.getElementById("courseNSection").innerHTML = "Inputting: Course" + course + " Section" + section;
+        }
+    </script>   
  
     <script type="text/javascript">
-        var k = "The respective values are :";
-        function planner() {
-            var course1section1[] = document.getElementsByName('course1section1[]');
-            var course1section2[] = document.getElementsByName('course1section2[]');
-            var course2section1[] = document.getElementsByName('course2section1[]');
-            var course2section2[] = document.getElementsByName('course2section2[]');
-            //not on the same day or course2 start time after course1 end time or reversely;
-            if(course1section1[0] != course2section1[0] || course2section1[1] > course1section1[2] || course1section1[1] > course2section1[2]){
-                k = "You can choose course1 section1 and course2 section1";
+        var k = "";
+        function planner(index, ans) {
+            if(index == courses.length){
+                for(var i = 0; i < ans.length; ++i){
+                    k += "Course" + (i + 1) + "Section" + ans[i] + " ";
+                }
             }
-            else if(course1section2[0] != course2section1[0] || course2section1[1] > course1section2[2] || course1section2[1] > course2section1[2]){
-                k = "You can chose course1 section2 and course2 section1"
+            for(var i = 0; i < courses[index].length; ++i){
+                var available = 1;
+                for(var j = 0; j < ans.length; ++j){
+                    if(courses[index][0] == ans[j][0] && 
+                        (ans[j][1] < courses[index][2] && ans[j][2] > courses[index][1])||
+                        (courses[index][1] < ans[j][2] && courses[index][2] > ans[j][1])){
+                            available = 0;
+                            break;
+                        }
+                }
+                if(available){
+                    ans.push(courses[index][i]);
+                    planner(index + 1, ans);
+                    ans.pop();
+                }
             }
-            else if(course1section1[0] != course2section2[0] || course1section1[1] > course2section2[2] || course2section2[1] > course1section1[2]){
-                k = "You can chose course1 section1 and course2 section2"
+            if(k == ""){
+                k = "No arrangement available";
             }
-            else if(course1section2[0] != course2section1[0] || course2section2[1] > course1section2[2] || course1section2[1] > course2section2[2]){
-                k = "You can chose course1 section2 and course2 section2"
-            }
- 
-            document.getElementById("par").innerHTML = k;
         }
+        document.getElementById("par").innerHTML = k;
     </script>
 </body>
  
